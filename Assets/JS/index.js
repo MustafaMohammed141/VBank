5db1c5940cb261d51cea6e8224784a453aca398d
MustafaMohammed141 <mostafamohamed12434@gmail.com>
2025-01-11 03:24:20 +0200

    top button animation

--- Assets/JS/index.js
+++ Assets/JS/index.js
@@ -1,14 +1,27 @@
+//* ---- Scrolling ---- *//
 const abtLnk = document.getElementById("abtLnk");
 const homeBtn = document.getElementById("homeBtn");
 
 abtLnk.addEventListener("click", function (event) {
-	Scroll(event, "aboutUs");
+	ScrollBtn(event, "aboutUs");
 });
 homeBtn.addEventListener("click", function (event) {
-	Scroll(event, "main");
+	ScrollBtn(event, "main");
 });
 
-function Scroll(event, eId) {
+function ScrollBtn(event, eId) {
 	event.preventDefault();
 	document.getElementById(eId).scrollIntoView({ behavior: "smooth" });
 }
+//* ---- Hidding ---- *//
+const mainSec = document.getElementById("main");
+
+window.addEventListener("scroll", function () {
+	const triggerPosition = mainSec.offsetHeight / 2;
+
+	if (window.scrollY >= triggerPosition) {
+		homeBtn.style.transform = "scale(1)";
+	} else {
+		homeBtn.style.transform = "scale(0)";
+	}
+});
